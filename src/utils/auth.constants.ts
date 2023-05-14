44ffc71ff939351dc853b13181eef3f256d5f540
sara1404 <sara14042000@gmail.com>
2023-05-15 01:26:43 +0200

    register user implemented

--- src/utils/auth.constants.ts
+++ src/utils/auth.constants.ts
@@ -1,39 +1,48 @@
-import * as yup from "yup"
+import * as yup from "yup";
 
 export const LOGIN_VALIDATION_SCHEMA = yup.object({
-    email: yup.string().required().email(),
-    password: yup.string().required().min(8)
-})
+  email: yup.string().required().email(),
+  password: yup.string().required().min(8),
+});
 
 export const LOGIN_DEFAULT_VALUES = {
-    email: "",
-    password: ""
-}
+  email: "",
+  password: "",
+};
 
 export const REGISTER_VALIDATION_SCHEMA = yup.object({
-    email: yup.string().required().email(),
-    password: yup.string().required().min(8),
-    confirmPassword: yup.string().required().min(8),
-    name: yup.string().required(),
-    surname: yup.string().required(),
-    phoneNumber: yup.string().required(),
-    address: yup.object().shape({
-        street: yup.string().required(),
-        streetNumber: yup.string().required(),
-        city: yup.string().required(),
-        zipCode: yup.string().required(),
-        country: yup.string().required()
-    })
-})
+  email: yup.string().required().email(),
+  password: yup
+    .string()
+    .required()
+    .min(8)
+    .matches(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "Password must be at least 8 characters long, and include one digit, one uppercase letter, one lowercase letter, and one special character"
+    ),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "Passwords must match"),
+  name: yup.string().required(),
+  surname: yup.string().required(),
+  phoneNumber: yup.string().required(),
+  address: yup.object().shape({
+    street: yup.string().required(),
+    streetNumber: yup.string().required(),
+    city: yup.string().required(),
+    zipCode: yup.string().required(),
+    country: yup.string().required(),
+  }),
+});
 
 export const REGISTER_DEFAULT_VALUES = {
-    email: "",
-    password: "",
-    confirmPassword: "",
-    name: "",
-    surname: "",
-    phoneNumber: "",
-}
+  email: "",
+  password: "",
+  confirmPassword: "",
+  name: "",
+  surname: "",
+  phoneNumber: "",
+};
 export const DEFAULT_HEADERS = {
-    'Content-Type': 'application/json'
-}
+  "Content-Type": "application/json",
+};
