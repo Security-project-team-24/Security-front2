8b428f900fb8f7a6ae4a231d975cc3877c4294ba
sara1404 <sara14042000@gmail.com>
2023-05-27 12:07:07 +0200

    update engineer skill updated

--- src/api/services/user/useUpdateSkill.tsx
+++ src/api/services/user/useUpdateSkill.tsx
@@ -3,28 +3,25 @@ import { useResponseState } from '../../useResponseState';
 import { Skill } from './types/skill';
 import { toast } from 'react-toastify';
 
-export const useAddSkill = () => {
+export const useUpdateSkill = () => {
   const { axios } = useAxios();
   const {
     setError,
     setLoading,
     setSuccess,
-    state: addSkillRes,
+    state: updateSkillRes,
   } = useResponseState<any>(null);
 
-  const addSkill = async ({ skills, strength }: Skill) => {
+  const updateSkill = async (skill: Skill) => {
     try {
       setLoading();
-      const res = await axios.post(`/user/skill`, {
-        skills: [
-          {
-            skill: skills,
-            strength: strength,
-          },
-        ],
+      const res = await axios.patch(`/user/skill`, {
+        id: skill.id,
+        skill: skill.skill,
+        strength: skill.strength,
       });
       setSuccess(res.data);
-      toast.success('Skill successfully added!');
+      toast.success('Skill successfully updated!');
     } catch (e: any) {
       setError(e.response.data.message);
       toast.error(e.response.data.message);
@@ -32,7 +29,7 @@ export const useAddSkill = () => {
   };
 
   return {
-    addSkillRes,
-    addSkill,
+    updateSkillRes,
+    updateSkill,
   };
 };
